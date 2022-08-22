046b54a4c53b11f9547e86a04ad0441ebd104690
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2022-08-22 19:26:38 -0300

    Criando uma página de profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage' }
+      { title: 'Categoria', component: 'CategoriasPage' },
+      { title: 'Profile', component: 'ProfilePage' }
     ];
 
   }
