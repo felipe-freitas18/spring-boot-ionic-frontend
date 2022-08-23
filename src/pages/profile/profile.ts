278d6d9c45f0ab56895957cb136192e2251fbf61
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2022-08-22 22:02:45 -0300

    Mostrando dados e imagem do cliente para página de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,3 +1,6 @@
+import { API_CONFIG } from './../../config/api.config';
+import { ClienteService } from './../../services/cliente.service';
+import { ClienteDTO } from './../../models/cliente.dto';
 import { StorageService } from './../../services/storage.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -9,19 +12,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  email: string;
+  cliente: ClienteDTO;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public storage: StorageService) {
+    public storage: StorageService,
+    public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
-        this.email = localUser.email;
+        this.clienteService.findByEmail(localUser.email)
+        .subscribe(response =>{
+          this.cliente = response;
+          this.getImageIfExists();
+        },
+        error => {})
     }
   }
 
+  getImageIfExists(){
+    this.clienteService.getImageFromBucket(this.cliente.id)
+    .subscribe(response=> {
+      this.cliente.imageURL = `${API_CONFIG.bucketBase_Url}/cp${this.cliente.id}.jpg`; 
+    },
+    error => {});
+  }
 }
