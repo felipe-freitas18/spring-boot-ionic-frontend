67e7ca7b1027f75f619f7da3afa0793585e4e0ce
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2022-08-25 08:45:46 -0300

    Carregando dados do produto

--- src/services/produto.service.ts
+++ src/services/produto.service.ts
@@ -1,3 +1,4 @@
+import { ProdutoDTO } from './../models/produto.dto';
 import { API_CONFIG } from '../config/api.config';
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
@@ -9,6 +10,10 @@ export class ProdutoService {
     constructor(public http: HttpClient){
     }
 
+    findById(produto_id: string)  {
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.base_Url}/produtos/${produto_id}`);
+    }
+
     findAllByCategoria(categoria_id: string)  {
         return this.http.get(`${API_CONFIG.base_Url}/produtos?categorias=${categoria_id}`);
     }
@@ -18,4 +23,8 @@ export class ProdutoService {
         return this.http.get(url, {responseType: 'blob'});
     }
 
+    getImageFromBucket(id: string) : Observable<any>{
+        let url = `${API_CONFIG.bucketBase_Url}/prod${id}.jpg`;
+        return this.http.get(url, {responseType: 'blob'});
+    }
 }
