22629d822c16c594589fcf872bd5599580ec2ee7
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2022-08-29 19:57:05 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    base_Url: "http://localhost:8080",
+    base_Url: "https://curso-udemy-springboot-ionic.herokuapp.com",
     bucketBase_Url: "https://ffreitas-curso-spring-ionic.s3.sa-east-1.amazonaws.com"
 }
