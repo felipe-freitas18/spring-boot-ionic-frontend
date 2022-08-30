820187861e7db22f3791f928dca0a8dbff140835
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2022-08-29 23:34:43 -0300

    bugfix - atualizacao da imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { StorageService } from './../../services/storage.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -16,14 +17,18 @@ export class ProfilePage {
   cliente: ClienteDTO;
   picture : string;
   cameraOn: boolean = false;
+  profileImage;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService,
-    public camera: Camera
+    public camera: Camera,
+    public sanitizer: DomSanitizer
     ) {
+
+      this.profileImage = '../../assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -53,10 +58,25 @@ export class ProfilePage {
     this.clienteService.getImageFromBucket(this.cliente.id)
     .subscribe(response=> {
       this.cliente.imageURL = `${API_CONFIG.bucketBase_Url}/cp${this.cliente.id}.jpg`; 
+      this.blobToDataURL(response).then(dataUrl=> {
+        let str : string = dataUrl as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      })
     },
-    error => {});
+    error => {
+      this.profileImage = '../../assets/imgs/avatar-blank.png';
+    });
   }
   
+   // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+   blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+        let reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = (e) => fulfill(reader.result);
+        reader.readAsDataURL(blob);
+    })
+  }
   
   getCameraPicture() {
     this.cameraOn = true;
@@ -102,7 +122,7 @@ export class ProfilePage {
     this.clienteService.uploadPicture(this.picture)
     .subscribe(response => {
       this.picture = null;
-      this.loadData();
+      this.getImageIfExists();
     }, 
     error =>{})
   }
